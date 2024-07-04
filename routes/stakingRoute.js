e8812a42a4037a42e42ead1d6432dfd0d4a8aaa9
Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-07-04 22:33:17 +0100

    remove admin from route

--- routes/stakingRoute.js
+++ routes/stakingRoute.js
@@ -9,7 +9,7 @@ router.post("/unstake/:userId", stakingController.unstake);
 router.post("/withdraw", stakingController.withdraw);
 router.post("/claim-nac-reward/:userId", stakingController.claimNacReward);
 router.post("/claim-dai-reward/:userId", stakingController.claimDaiReward); // claim dai monthly reward
-router.put("/update-staking-config", ensureAdmin, stakingController.updateStakingConfig);
+router.put("/update-staking-config", stakingController.updateStakingConfig);
 router.get("/staking-config", stakingController.getStakingConfig);
 router.get("/user-stakes/:userId", stakingController.getUserStakes);
 router.get("/available-nac-reward/:userId", stakingController.getAvailableNacReward);
